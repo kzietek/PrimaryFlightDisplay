8b2630d67d6c90fb59a6f6d442018bbeda04b460
Michael Koukoullis <mike@kouky.org>
2016-02-29 19:04:32 +1100

    Initial iOS framework project

---
+++ PrimaryFlightDisplay/PrimaryFlightDisplay.h
@@ -0,0 +1,19 @@
+//
+//  PrimaryFlightDisplay.h
+//  PrimaryFlightDisplay
+//
+//  Created by Michael Koukoullis on 29/02/2016.
+//  Copyright © 2016 Michael Koukoullis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrimaryFlightDisplay.
+FOUNDATION_EXPORT double PrimaryFlightDisplayVersionNumber;
+
+//! Project version string for PrimaryFlightDisplay.
+FOUNDATION_EXPORT const unsigned char PrimaryFlightDisplayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrimaryFlightDisplay/PublicHeader.h>
+
+
