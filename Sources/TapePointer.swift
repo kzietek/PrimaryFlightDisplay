e3256dc2e881163d86c5afcb28485d8eb226cdf7
Michael Koukoullis <mike@kouky.org>
2016-03-06 22:08:45 +1100

    Legend labels for altimeter and airspeed indicator

--- Sources/TapePointer.swift
+++ Sources/TapePointer.swift
@@ -7,28 +7,44 @@
 //
 
 import SpriteKit
+import Darwin
 
 class TapePointer: SKNode {
     
-
-    var style: TapeIndicatorStyleType
-    private let label: SKLabelNode
+    let style: TapeIndicatorStyleType
+    private let valueLabel: SKLabelNode
+    private let legendKeyLabelNode: SKLabelNode?
+    private let legendValueLabelNode: SKLabelNode?
     
     var value: Int {
         didSet {
-            label.text = style.labelForValue(value)
+            valueLabel.text = style.labelForValue(value)
         }
     }
     
     init(initialValue: Int, style: TapeIndicatorStyleType) {
         self.value = initialValue
         self.style = style
-        label = SKLabelNode(text: style.labelForValue(value))
+        valueLabel = SKLabelNode(text: style.labelForValue(value))
+        if let legend = style.legend {
+            legendKeyLabelNode = SKLabelNode(text: legend.key)
+            legendValueLabelNode = SKLabelNode(text: legend.value)
+        } else {
+            legendKeyLabelNode = nil
+            legendValueLabelNode = nil
+        }
+        
         super.init()
         
         addChild(buildBackgroundShape())
         styleLabelNode()
-        addChild(label)
+        addChild(valueLabel)
+        if let node = legendKeyLabelNode {
+            addChild(node)
+        }
+        if let node = legendValueLabelNode {
+            addChild(node)
+        }
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -36,66 +52,90 @@ class TapePointer: SKNode {
     }
     
     private func styleLabelNode() {
-        label.fontName = style.font.family
-        label.fontSize = style.font.size
-        label.color = style.markerTextColor
-        
+        valueLabel.fontName = style.font.family
+        valueLabel.fontSize = style.font.size
+        valueLabel.fontColor = style.markerTextColor
+
+        legendKeyLabelNode?.fontName = style.font.family
+        legendKeyLabelNode?.fontSize = round(style.font.size * CGFloat(0.5))
+        legendKeyLabelNode?.fontColor = style.markerTextColor.colorWithAlphaComponent(0.5)
+
+        legendValueLabelNode?.fontName = style.font.family
+        legendValueLabelNode?.fontSize = round(style.font.size * CGFloat(0.5))
+        legendValueLabelNode?.fontColor = style.markerTextColor.colorWithAlphaComponent(0.5)
+
         switch style.markerJustification {
         case .Top:
-            label.horizontalAlignmentMode = .Center
-            label.verticalAlignmentMode = .Top
-            label.position = CGPoint(x: 0, y: style.size.height/2 - CGFloat(style.markerTextOffset))
+            valueLabel.horizontalAlignmentMode = .Center
+            valueLabel.verticalAlignmentMode = .Top
+            valueLabel.position = CGPoint(x: 0, y: style.size.height/2 - CGFloat(style.markerTextOffset))
         case .Bottom:
-            label.horizontalAlignmentMode = .Center
-            label.verticalAlignmentMode = .Bottom
-            label.position = CGPoint(x: 0, y: CGFloat(style.markerTextOffset) - style.size.height/2)
+            valueLabel.horizontalAlignmentMode = .Center
+            valueLabel.verticalAlignmentMode = .Bottom
+            valueLabel.position = CGPoint(x: 0, y: CGFloat(style.markerTextOffset) - style.size.height/2)
         case .Left:
-            label.horizontalAlignmentMode = .Left
-            label.verticalAlignmentMode = .Center
-            label.position = CGPoint(x: CGFloat(style.markerTextOffset) - style.size.width/2, y: 0)
+            valueLabel.horizontalAlignmentMode = .Left
+            valueLabel.verticalAlignmentMode = .Center
+            valueLabel.position = CGPoint(
+                x: CGFloat(style.markerTextOffset) - style.size.width/2,
+                y: 0)
+            
+            legendKeyLabelNode?.horizontalAlignmentMode = .Left
+            legendKeyLabelNode?.verticalAlignmentMode = .Top
+            legendKeyLabelNode?.position = CGPoint(
+                x: CGFloat(style.markerTextOffset) - style.size.width/2,
+                y: backgroundShapeDimensions().thirdWidth)
+            
+            legendValueLabelNode?.horizontalAlignmentMode = .Left
+            legendValueLabelNode?.verticalAlignmentMode = .Bottom
+            legendValueLabelNode?.position = CGPoint(
+                x: CGFloat(style.markerTextOffset) - style.size.width/2,
+                y: -backgroundShapeDimensions().thirdWidth)
         case .Right:
-            label.horizontalAlignmentMode = .Right
-            label.verticalAlignmentMode = .Center
-            label.position = CGPoint(x: style.size.width/2 - CGFloat(style.markerTextOffset), y: 0)
+            valueLabel.horizontalAlignmentMode = .Right
+            valueLabel.verticalAlignmentMode = .Center
+            valueLabel.position = CGPoint(
+                x: style.size.width/2 - CGFloat(style.markerTextOffset),
+                y: 0)
+
+            legendKeyLabelNode?.horizontalAlignmentMode = .Right
+            legendKeyLabelNode?.verticalAlignmentMode = .Top
+            legendKeyLabelNode?.position = CGPoint(
+                x: style.size.width/2 - CGFloat(style.markerTextOffset),
+                y: backgroundShapeDimensions().thirdWidth)
+            
+            legendValueLabelNode?.horizontalAlignmentMode = .Right
+            legendValueLabelNode?.verticalAlignmentMode = .Bottom
+            legendValueLabelNode?.position = CGPoint(
+                x: style.size.width/2 - CGFloat(style.markerTextOffset),
+                y: -backgroundShapeDimensions().thirdWidth)
         }
     }
     
     private func buildBackgroundShape() -> SKShapeNode {
-        let width, halfWidth, thirdWidth: CGFloat
-        
-        switch style.markerJustification {
-        case .Top, .Bottom:
-            width = CGFloat(style.size.height)
-            halfWidth = width / 2
-            thirdWidth = width / 3
-        case .Left, .Right:
-            width = style.size.width
-            halfWidth = width / 2
-            thirdWidth = width / 3
-        }
-        
+        let dimensions = backgroundShapeDimensions()
         let path = CGPathCreateMutable()
         CGPathMoveToPoint(path, nil, 0, 0)
-        CGPathAddLineToPoint(path, nil, thirdWidth, thirdWidth)
-        CGPathAddLineToPoint(path, nil, width, thirdWidth)
-        CGPathAddLineToPoint(path, nil, width, -thirdWidth)
-        CGPathAddLineToPoint(path, nil, thirdWidth, -thirdWidth)
+        CGPathAddLineToPoint(path, nil, dimensions.thirdWidth, dimensions.thirdWidth)
+        CGPathAddLineToPoint(path, nil, dimensions.width, dimensions.thirdWidth)
+        CGPathAddLineToPoint(path, nil, dimensions.width, -dimensions.thirdWidth)
+        CGPathAddLineToPoint(path, nil, dimensions.thirdWidth, -dimensions.thirdWidth)
         CGPathCloseSubpath(path)
         
         let translateTransform, rotateTransform: CGAffineTransform
 
         switch style.markerJustification {
         case .Top:
-            translateTransform = CGAffineTransformMakeTranslation(-halfWidth, 0)
+            translateTransform = CGAffineTransformMakeTranslation(-dimensions.halfWidth, 0)
             rotateTransform = CGAffineTransformMakeRotation(CGFloat(-M_PI_2))
         case .Bottom:
-            translateTransform = CGAffineTransformMakeTranslation(-halfWidth, 0)
+            translateTransform = CGAffineTransformMakeTranslation(-dimensions.halfWidth, 0)
             rotateTransform = CGAffineTransformMakeRotation(CGFloat(M_PI_2))
         case .Left:
-            translateTransform = CGAffineTransformMakeTranslation(-halfWidth, 0)
+            translateTransform = CGAffineTransformMakeTranslation(-dimensions.halfWidth, 0)
             rotateTransform = CGAffineTransformIdentity
         case .Right:
-            translateTransform = CGAffineTransformMakeTranslation(-halfWidth, 0)
+            translateTransform = CGAffineTransformMakeTranslation(-dimensions.halfWidth, 0)
             rotateTransform = CGAffineTransformMakeRotation(CGFloat(M_PI))
         }
         
@@ -108,5 +148,22 @@ class TapePointer: SKNode {
         shape.fillColor = style.pointerBackgroundColor
         shape.strokeColor = SKColor.whiteColor()
         return shape
+    }
+    
+    private func backgroundShapeDimensions() -> (width: CGFloat, halfWidth: CGFloat, thirdWidth: CGFloat) {
+        let width, halfWidth, thirdWidth: CGFloat
+        
+        switch style.markerJustification {
+        case .Top, .Bottom:
+            width = CGFloat(style.size.height)
+            halfWidth = width / 2
+            thirdWidth = width / 3
+        case .Left, .Right:
+            width = style.size.width
+            halfWidth = width / 2
+            thirdWidth = width / 3
+        }
+
+        return (width, halfWidth, thirdWidth)
     }
 }
